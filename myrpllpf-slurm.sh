4de267d97ffd72e7475b2a52dae4af45c3cdc486
AWS ParallelCluster user <ec2-user@ip-10-0-9-102.ap-southeast-1.compute.internal>
2020-10-31 09:26:44 +0000

    adding lab 7 files

---
+++ myrpllpf-slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Submit this script with: sbatch <this-filename>
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH -J "rpllpf"   # job name
+
+## /SBATCH -p general # partition (queue)
+#SBATCH -o rpllpf-slurm.%N.%j.out # STDOUT
+#SBATCH -e rpllpf-slurm.%N.%j.err # STDERR
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+python -u -c "import PyHipp as pyh; import time; pyh.RPLLFP(saveLevel=1); print(time.localtime());"
